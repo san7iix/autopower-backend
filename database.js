8cf2079fabb5eb0d17779fb0935925908d87ab81
Santiago <xsantysxx@gmail.com>
2021-08-06 14:28:40 -0500

    agregada db

--- database.js
+++ database.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
 
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Mimondaca12!',
-    database: 'auto_power',
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'b0adf71c13480f',
+    password: '14152889',
+    database: 'heroku_d29a844f755278c',
     multipleStatements: true
 });
+
+//mysql://b0adf71c13480f:14152889@us-cdbr-east-04.cleardb.com/heroku_d29a844f755278c?reconnect=true
 
 mysqlConnection.connect(function(err) {
     if (err) {
